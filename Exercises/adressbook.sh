b5c0fac6e16e0ca1f58f51cbe3fbde11207a147f
Guilhermo Masid <guilhermomasid@gmail.com>
2021-11-08 15:26:52 -0300

    create exercises folder with specifications description

---
+++ Exercises/adressbook.sh
@@ -0,0 +1,17 @@
+search() {
+  # Find records with parameter field:parameter given
+}
+
+add() {
+  # Create a record with: Id, Name, Surname, Email and Phone
+  # 192:John Smith:54321:john@smith.example.com:19264892
+}
+
+update() {
+  # Update given param to newone based on adress book item id
+  # Ex: update 192 phone 249833966 (Update item with id 192 phone to be 249833966)
+}
+
+remove() {
+  # Delete item in list with given id (confirm before delete)
+}
